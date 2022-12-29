3ae07937e4c24af5b9cbac38dab783114fd8885c
Lukáš Lokša <86780297+Powissen@users.noreply.github.com>
2022-12-29 12:54:39 +0100

    v1.0 - Basic functions of browser

---
+++ WebTest/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace WebTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.defaultScreenSearch = new System.Windows.Forms.TextBox();
+            this.defaultScreen = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.defaultScreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.BackColor = System.Drawing.Color.DarkGray;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // progressBar1
+            // 
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.ForeColor = System.Drawing.Color.Transparent;
+            this.progressBar1.Name = "progressBar1";
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            resources.ApplyResources(this.webView, "webView");
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Name = "webView";
+            this.webView.ZoomFactor = 1D;
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // defaultScreenSearch
+            // 
+            resources.ApplyResources(this.defaultScreenSearch, "defaultScreenSearch");
+            this.defaultScreenSearch.BackColor = System.Drawing.Color.Gainsboro;
+            this.defaultScreenSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.defaultScreenSearch.ForeColor = System.Drawing.Color.DimGray;
+            this.defaultScreenSearch.Name = "defaultScreenSearch";
+            this.defaultScreenSearch.Tag = "Search or Type URL";
+            this.defaultScreenSearch.MouseClick += new System.Windows.Forms.MouseEventHandler(this.defaultScreenSearch_MouseClick);
+            this.defaultScreenSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.defaultScreenSearch_KeyDown_1);
+            // 
+            // defaultScreen
+            // 
+            resources.ApplyResources(this.defaultScreen, "defaultScreen");
+            this.defaultScreen.Name = "defaultScreen";
+            this.defaultScreen.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click_1);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.pictureBox3, "pictureBox3");
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.defaultScreenSearch);
+            this.Controls.Add(this.defaultScreen);
+            this.Controls.Add(this.webView);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.defaultScreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        private System.Windows.Forms.TextBox defaultScreenSearch;
+        private System.Windows.Forms.PictureBox defaultScreen;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ WebTest/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+
+namespace WebTest
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint ="CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+            (
+                int nLeft,
+                int nTop,
+                int nRight,
+                int nBottom,
+                int nWidthEllipse,
+                int nHeightEllipse
+            );
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitBrowser();
+            progressBar1.ForeColor = Color.Gray;
+            progressBar1.Style = ProgressBarStyle.Continuous;
+        }
+        private async Task initizated()
+        {
+            await webView.EnsureCoreWebView2Async(null);
+        }
+        public async void InitBrowser()
+        {
+            await initizated();
+            webView.CoreWebView2.Navigate("https://www.google.com/");
+        }
+
+
+
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            webView.CoreWebView2.Navigate("https://www.google.com/");
+            textBox1.Text = "https://www.google.com/";
+            defaultScreen.Visible = true;
+            defaultScreenSearch.Visible = true;
+            defaultScreenSearch.ForeColor = Color.DimGray;
+            defaultScreenSearch.Text = "Search or Type URL";
+        }
+
+        private void pictureBox2_Click_1(object sender, EventArgs e)
+        {
+            webView.GoForward();
+            LoadingAnimation();
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            webView.GoBack();
+            LoadingAnimation();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (textBox1.Text.StartsWith("http") && textBox1.Text.Contains(".") || textBox1.Text.StartsWith("https") && textBox1.Text.Contains("."))
+                {
+                    webView.CoreWebView2.Navigate(textBox1.Text);
+                }
+                else if (textBox1.Text.Contains("."))
+                {
+                    int count = 0;
+                    foreach (char i in textBox1.Text)
+                    {
+                        if (i == '.')
+                            count++;
+                    }
+                    if (count == 1)
+                    {
+                        webView.CoreWebView2.Navigate("https://" + textBox1.Text);
+                        textBox1.Text = "https://" + textBox1.Text;
+                    }
+                    else
+                    {
+                        webView.CoreWebView2.Navigate("https://www.google.com/search?q=" + textBox1.Text);
+                        textBox1.Text = "https://www.google.com/search?q=" + textBox1.Text;
+                    }
+                }
+                else
+                {
+                    webView.CoreWebView2.Navigate("https://www.google.com/search?q=" + textBox1.Text);
+                    textBox1.Text = "https://www.google.com/search?q=" + textBox1.Text;
+                }
+
+                LoadingAnimation();
+            }
+        }
+        
+        private void defaultScreenSearch_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (defaultScreenSearch.Text.StartsWith("http") && defaultScreenSearch.Text.Contains(":") || defaultScreenSearch.Text.StartsWith("https") && defaultScreenSearch.Text.Contains(":"))
+                {
+                    webView.CoreWebView2.Navigate(defaultScreenSearch.Text);
+                    textBox1.Text = defaultScreenSearch.Text;
+                }
+                else if (defaultScreenSearch.Text.Contains("."))
+                {
+                    int count = 0;
+                    foreach (char i in defaultScreenSearch.Text)
+                    {
+                        if (i == '.')
+                            count++;
+                    }
+                    if (count == 1)
+                    {
+                        webView.CoreWebView2.Navigate("https://" + defaultScreenSearch.Text);
+                        textBox1.Text = "https://" + defaultScreenSearch.Text;
+                    }
+                    else
+                    {
+                        webView.CoreWebView2.Navigate("https://www.google.com/search?q=" + defaultScreenSearch.Text);
+                        textBox1.Text = "https://www.google.com/search?q=" + defaultScreenSearch.Text;
+                    }
+                }
+                else
+                {
+                    webView.CoreWebView2.Navigate("https://www.google.com/search?q=" + defaultScreenSearch.Text);
+                    textBox1.Text = "https://www.google.com/search?q=" + defaultScreenSearch.Text;
+                }
+                textBox1.SelectionStart = 0;
+
+                LoadingAnimation();
+
+                defaultScreen.Visible = false;
+                defaultScreenSearch.Visible = false;
+            }
+        }
+
+        private void defaultScreenSearch_MouseClick(object sender, MouseEventArgs e)
+        {
+            defaultScreenSearch.ForeColor = Color.Black;
+            defaultScreenSearch.Text = "";
+        }
+
+        void LoadingAnimation()
+        {
+            progressBar1.Visible = true;
+            for (int i = 0; i < 101; i++)
+            {
+                progressBar1.Value = i;
+                Thread.Sleep(2);
+            }
+            progressBar1.Visible = false;
+        }
+    }
+}
